3765d535f601e0a7d9f5f1213970e674a7db06f3
Tanishq Singla <tanishqsngl@gmail.com>
2023-07-10 14:55:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My parents new Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
